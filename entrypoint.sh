21a4cfa8f4fa3c639ad7653e29ea94e873ffcd2f
lnterface <lnterface@users.noreply.github.com>
2016-12-13 16:31:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+PASS=${ROOT_PASS:-$(pwgen -s 12 1)}
+_word=$( [ ${ROOT_PASS} ] && echo "preset" || echo "random" )
+echo "=> Setting a ${_word} password to the root user"
+echo "root:$PASS" | chpasswd
+
+echo "=> Done!"
+
+echo "========================================================================"
+echo "You can now connect to this CentOS container via SSH using:"
+echo ""
+echo "    ssh -p <port> root@<host>"
+echo "and enter the root password '$PASS' when prompted"
+echo ""
+echo "Please remember to change the above password as soon as possible!"
+echo "========================================================================"
+
 nohup /usr/sbin/sshd -D &
 nohup /usr/local/bin/net_speeder eth0 "ip" >/dev/null 2>&1 &
 python /root/ssr/shadowsocks/server.py "$@"
